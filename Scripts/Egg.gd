f8f2f680e8e9191db94566fe9171a1901dfb44ce
dantoso <80010223+dantoso@users.noreply.github.com>
2024-07-21 16:44:05 -0300

    feat: coyote time

--- Scripts/Egg.gd
+++ Scripts/Egg.gd
@@ -4,13 +4,67 @@ class_name Egg
 @export var turnIntensity: = 500.0
 @export var acceleration: = 250.0
 @export var jump: = 500
+@export var resistance: = 300
+@export var coyoteTime: = 0.2
+
+@onready var timer: Timer = $Timer
+
+var prevVelocity: = Vector2.ZERO
+var currentVelocity: = Vector2.ZERO
+var lastCollision: KinematicCollision2D = null
+var canJump: = true
+var isJumping = false
+
+func _ready() -> void:
+	contact_monitor = true
+	max_contacts_reported = 18
+	
+	timer.wait_time = coyoteTime
+	timer.timeout.connect(
+		func():
+			canJump = false
+	)
 
 func _physics_process(delta: float) -> void:
 	var input: = Input.get_vector("left", "right", "up", "down")
 	apply_torque(input.length() * turnIntensity * input.x)
 	apply_central_force(Vector2(acceleration * input.x, 0))
+	var collision: = move_and_collide(Vector2.ZERO)
+	if collision:
+		var result: = collision.get_normal().dot(prevVelocity)
+		if result * sign(result) > resistance:
+			print("DEAD: ", result)
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("jump"):
+	if event.is_action_pressed("jump") && canJump:
+		canJump = false
 		apply_impulse(Vector2(0, -jump))
+
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	for i in state.get_contact_count():
+		if state.get_contact_local_normal(i).y <= -0.6:
+			canJump = true
+			timer.start()
+			break
+	
+	prevVelocity = currentVelocity
+	currentVelocity = state.linear_velocity
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
